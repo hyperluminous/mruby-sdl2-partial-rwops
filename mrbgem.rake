5111fab1229d2e6f599c3f7a37043503f2fa5a9f
kabies <37025477+kabies@users.noreply.github.com>
2018-04-03 01:54:06 +0900

    add ttf and music support. rwops structure close by load function.

--- mrbgem.rake
+++ mrbgem.rake
@@ -6,10 +6,11 @@ MRuby::Gem::Specification.new('mruby-sdl2-partial-rwops') do |spec|
   spec.add_dependency('mruby-sdl2')
   spec.add_dependency('mruby-sdl2-image')
   spec.add_dependency('mruby-sdl2-mixer')
+  spec.add_dependency('mruby-sdl2-ttf')
 
   if not RUBY_PLATFORM.include?('darwin') and not build.kind_of?(MRuby::CrossBuild)
     spec.cc.flags << '`sdl2-config --cflags`'
     spec.linker.flags_before_libraries << '`sdl2-config --libs`'
-    spec.linker.libraries += %w(SDL2_image SDL2_mixer)
+    spec.linker.libraries += %w(SDL2_image SDL2_mixer SDL2_ttf)
   end
 end
--- src/sdl2_partial_rwops.c
+++ src/sdl2_partial_rwops.c
@@ -5,11 +5,21 @@
 #include "sdl2_render.h"
 #include "sdl2_surface.h"
 #include "sdl2-mixer.h"
+#include "sdl2-ttf.h"
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
+static int myclose(SDL_RWops * context)
+{
+    free(context->hidden.unknown.data1);
+    if (context) {
+        SDL_FreeRW(context);
+    }
+    return 0;
+}
 
-SDL_RWops* new_partial_file_rwops(char* file, int start, int size)
+static SDL_RWops* new_partial_file_rwops(char* file, int start, int size)
 {
   SDL_RWops *file_rw;
   SDL_RWops* partial_rw;
@@ -22,14 +32,10 @@ SDL_RWops* new_partial_file_rwops(char* file, int start, int size)
   SDL_RWread( file_rw, buffer, size, 1 );
   SDL_RWclose(file_rw);
   partial_rw = SDL_RWFromConstMem( buffer, size );
+  /* memorize buffer address, override close method. */
   partial_rw->hidden.unknown.data1 = buffer;
+  partial_rw->close = myclose;
   return partial_rw;
-}
-
-void free_partial_file_rwops(SDL_RWops *rwops)
-{
-  free(rwops->hidden.unknown.data1);
-  SDL_RWclose(rwops);
 }
 
 /*
@@ -48,8 +54,7 @@ mrb_sdl2_partial_rwops_renderer_load_partial(mrb_state *mrb, mrb_value self)
   mrb_get_args(mrb, "Sii", &file, &start, &size);
   rwops = new_partial_file_rwops(RSTRING_PTR(file), start, size);
   renderer = mrb_sdl2_video_renderer_get_ptr(mrb, self);
-  result = mrb_sdl2_video_texture(mrb, IMG_LoadTexture_RW(renderer, rwops, 0));
-  free_partial_file_rwops(rwops);
+  result = mrb_sdl2_video_texture(mrb, IMG_LoadTexture_RW(renderer, rwops, 1));
 
   return result;
 }
@@ -69,9 +74,8 @@ mrb_sdl2_partial_rwops_surface_load_partial(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "Sii", &file, &start, &size);
   rwops = new_partial_file_rwops(RSTRING_PTR(file), start, size);
-  surface = IMG_Load_RW(rwops, 0);
+  surface = IMG_Load_RW(rwops, 1);
   result = mrb_sdl2_video_surface(mrb, surface, true);
-  free_partial_file_rwops(rwops);
 
   return result;
 }
@@ -91,11 +95,53 @@ mrb_sdl2_partial_rwops_surface_load_partial(mrb_state *mrb, mrb_value self)
 
    mrb_get_args(mrb, "Sii", &file, &start, &size);
    rwops = new_partial_file_rwops(RSTRING_PTR(file), start, size);
-   chunk = Mix_LoadWAV_RW(rwops, 0);
+   chunk = Mix_LoadWAV_RW(rwops, 1);
    result = mrb_sdl2_chunk(mrb, chunk);
-   free_partial_file_rwops(rwops);
 
    return result;
+}
+
+/*
+ * Mixer Music
+ */
+
+static mrb_value
+mrb_sdl2_partial_rwops_music_load_partial(mrb_state *mrb, mrb_value self)
+{
+  mrb_value file;
+  mrb_int start, size;
+  Mix_Music *music;
+  SDL_RWops * rwops;
+  mrb_value result;
+
+  mrb_get_args(mrb, "Sii", &file, &start, &size);
+  rwops = new_partial_file_rwops(RSTRING_PTR(file), start, size);
+  music = Mix_LoadMUS_RW(rwops, 1);
+  result = mrb_sdl2_music(mrb, music);
+
+  return result;
+}
+
+/*
+ * Font
+ */
+
+static mrb_value
+mrb_sdl2_partial_rwops_font_load_partial(mrb_state *mrb, mrb_value self)
+{
+  mrb_value file;
+  mrb_int start, size;
+  mrb_int fontsize;
+  TTF_Font *font;
+  SDL_RWops * rwops;
+  mrb_value result;
+
+  mrb_get_args(mrb, "Siii", &file, &start, &size,&fontsize);
+  rwops = new_partial_file_rwops(RSTRING_PTR(file), start, size);
+  font = TTF_OpenFontRW(rwops, 1, fontsize);
+  result = mrb_sdl2_font(mrb, font);
+
+  return result;
 }
 
 /*
@@ -110,16 +156,24 @@ mrb_mruby_sdl2_partial_rwops_gem_init(mrb_state *mrb)
     struct RClass *class_Surface;
     struct RClass *mod_Mixer;
     struct RClass *class_Chunk;
+    struct RClass *class_Music;
+    struct RClass *mod_Ttf;
+    struct RClass *class_Font;
 
     mod_Video      = mrb_module_get_under(mrb, mod_SDL2,  "Video");
     class_Renderer = mrb_class_get_under (mrb, mod_Video, "Renderer");
     class_Surface  = mrb_class_get_under (mrb, mod_Video, "Surface");
     mod_Mixer      = mrb_module_get_under(mrb, mod_SDL2, "Mixer");
     class_Chunk    = mrb_class_get_under (mrb, mod_Mixer, "Chunk");
+    class_Music    = mrb_class_get_under (mrb, mod_Mixer, "Music");
+    mod_Ttf        = mrb_module_get_under(mrb, mod_SDL2, "TTF");
+    class_Font     = mrb_class_get_under (mrb, mod_Ttf, "Font");
 
     mrb_define_method(mrb, class_Renderer, "load_partial", mrb_sdl2_partial_rwops_renderer_load_partial, MRB_ARGS_REQ(3));
     mrb_define_class_method(mrb, class_Surface, "load_partial", mrb_sdl2_partial_rwops_surface_load_partial, MRB_ARGS_REQ(3));
     mrb_define_class_method(mrb, class_Chunk, "load_partial", mrb_sdl2_partial_rwops_chunk_load_partial, MRB_ARGS_REQ(3));
+    mrb_define_class_method(mrb, class_Music, "load_partial", mrb_sdl2_partial_rwops_music_load_partial, MRB_ARGS_REQ(3));
+    mrb_define_class_method(mrb, class_Font, "load_partial", mrb_sdl2_partial_rwops_font_load_partial, MRB_ARGS_REQ(4));
 }
 
 void
